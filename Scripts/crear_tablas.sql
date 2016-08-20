5a20f85b537fa76cf6b1a36f4d33b9436bdb0ab9
Sebastian Giraldo <sebgv524@gmail.com>
2016-08-20 12:37:48 -0500

    Cambio variables y borrado tablespace
    Se cambian los tipos de variable sql a mysql,y se borran los tablespace.

--- Scripts/crear_tablas.sql
+++ Scripts/crear_tablas.sql
@@ -6,57 +6,57 @@ DROP TABLE VENDEDOR;
 
 CREATE TABLE CLIENTE
   (
-    documento_cliente VARCHAR2 (15),
-    nombre            VARCHAR2 (40),
-    primer_apellido   VARCHAR2 (40),
-    segundo_apellido  VARCHAR2 (40) ,
-    telefono_cli      VARCHAR2 (15),
-    correo            VARCHAR2 (40)
-  )
-  tablespace ts_compratodo;
+    documento_cliente VARCHAR (15),
+    nombre            VARCHAR (40),
+    primer_apellido   VARCHAR (40),
+    segundo_apellido  VARCHAR (40) ,
+    telefono_cli      VARCHAR (15),
+    correo            VARCHAR (40)
+  );
+  
 
 CREATE TABLE PRODUCTO
   (
-    codigo_producto  NUMBER (5),
-    nombre_producto  VARCHAR2 (40),
-    descripcion      VARCHAR2 (40) ,
-    precio           NUMBER (10),
-    existencia_real  NUMBER (5),
-    disponible_venta NUMBER (5)
-  )
-  tablespace ts_compratodo;
+    codigo_producto  numeric (5),
+    nombre_producto  VARCHAR (40),
+    descripcion      VARCHAR (40) ,
+    precio           numeric (10),
+    existencia_real  numeric (5),
+    disponible_venta numeric (5)
+  );
+  
 
 
 CREATE TABLE PRODUCTO_VENTA
   (
-    PRODUCTO_codigo_producto NUMBER (5),
-    VENTA_codigo_venta       NUMBER (5),
-    cantidad                 NUMBER (2)
-  )
-  tablespace ts_compratodo;
+    PRODUCTO_codigo_producto numeric (5),
+    VENTA_codigo_venta       numeric (5),
+    cantidad                 numeric (2)
+  );
+  
 
 
 
 CREATE TABLE VENDEDOR
   (
-    documento               VARCHAR2 (15),
-    nombre                  VARCHAR2 (40),
-    primer_apellido         VARCHAR2 (40),
-    segundo_apellido        VARCHAR2 (40),
-    telefono                VARCHAR2 (15),
-    celular                 VARCHAR2 (15),
-    correo                  VARCHAR2 (40),
-    direccion               VARCHAR2 (40),
-    VENDEDOR_documento_jefe VARCHAR2 (15)
-  )
-  tablespace ts_compratodo;
+    documento               VARCHAR (15),
+    nombre                  VARCHAR (40),
+    primer_apellido         VARCHAR (40),
+    segundo_apellido        VARCHAR (40),
+    telefono                VARCHAR (15),
+    celular                 VARCHAR (15),
+    correo                  VARCHAR (40),
+    direccion               VARCHAR (40),
+    VENDEDOR_documento_jefe VARCHAR(15)
+  );
+  
 
 CREATE TABLE VENTA
   (
-    codigo_venta              NUMBER (5),
+    codigo_venta              numeric (5),
     fecha_venta               DATE,
-    valor_total               NUMBER (10),
-    VENDEDOR_documento        VARCHAR2 (15),
-    CLIENTE_documento_cliente VARCHAR2 (15)
-  )
-  tablespace ts_compratodo;
+    valor_total               numeric (10),
+    VENDEDOR_documento        VARCHAR (15),
+    CLIENTE_documento_cliente VARCHAR (15)
+  );
+  
