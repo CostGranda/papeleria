3772508965875794037910bfa34ecea625b467ba
Cost Granda <jorge_granda82112@elpoli.edu.co>
2016-08-22 00:39:54 -0500

    Forms
    Formularios base

--- PapeleriaTPA/src/papeleriatpa/LogicaDeNegocio.java
+++ PapeleriaTPA/src/papeleriatpa/LogicaDeNegocio.java
@@ -6,17 +6,41 @@
 package papeleriatpa;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 /**
- *  
+ *
  * @author Jorge L Granda.
  */
 public class LogicaDeNegocio {
 
     private Connection con;
 
+    /**
+     * Realiza la conexión inmediatamente se instancia la clase.
+     */
     public LogicaDeNegocio() {
         con = ConexionMySQL.establecerConexionSQL();
+    }
+    
+    /**
+     * Valida el usuario o vendedor que inicia sesión para controlar las ventas.
+     * @param usuario 
+     * @param clave 
+     * @return boolean
+     */
+    public boolean validarUsuario(String usuario, String clave) {
+        boolean b = false;
+        ResultSet rs = ConexionMySQL.validarUsuario(con, usuario, clave);
+        try {
+            if (rs.next()) {
+                b = true;
+            }
+        } catch (SQLException ex) {
+            System.out.println("Error: " + ex);
+        }
+        return b;
     }
 
 }
