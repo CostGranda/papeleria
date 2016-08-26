7155b17df1ce507fbb5e9c4556fc30e5351bc526
Cost Granda <jorge_granda82112@elpoli.edu.co>
2016-08-26 00:24:20 -0500

    Formulario Insertar
    Ya funciona el formulario insertar vendedor

--- PapeleriaTPA/src/Logica/Usuarios.java
+++ PapeleriaTPA/src/Logica/Usuarios.java
@@ -15,6 +15,7 @@ import java.sql.Statement;
  * @author sebgv_000
  */
 public class Usuarios {
+
     public static ResultSet validarUsuario(Connection con, String usuario, String clave) {
         ResultSet rs = null;
         Statement st;
@@ -27,5 +28,19 @@ public class Usuarios {
             System.out.println("Error: " + ex);
         }
         return rs;
+    }
+
+    public static int insertarLogin(Connection con, String usuario, String clave) {
+        Statement st;
+        int cantfilias = 0;
+        try {
+            st = con.createStatement();
+            String strSql;
+            strSql = "INSERT INTO `Papeleria`.`LOGIN` VALUES ('"+usuario+"','"+clave+"','vendedor');";
+            cantfilias = st.executeUpdate(strSql);
+        } catch (SQLException ex) {
+            System.out.println("\nError al INSERTAR: " + ex);
+        }
+        return cantfilias;
     }
 }
