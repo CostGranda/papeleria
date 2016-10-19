9e48fa29ec657bc97a1c47dc3eace605871b0027
Cost Granda <costeeleation@gmail.com>
2016-10-18 22:00:23 -0500

    Cursor
    Cursor de mano al boton olvidar.

--- PapeleriaTPA/src/Vista/Formularios/frmLogin.java
+++ PapeleriaTPA/src/Vista/Formularios/frmLogin.java
@@ -89,7 +89,7 @@ public class frmLogin extends javax.swing.JFrame {
         btnAceptar.setBorder(null);
         btnAceptar.setBorderPainted(false);
         btnAceptar.setContentAreaFilled(false);
-        btnAceptar.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+        btnAceptar.setCursor(new java.awt.Cursor(java.awt.Cursor.DEFAULT_CURSOR));
         btnAceptar.setFocusPainted(false);
         btnAceptar.setHorizontalTextPosition(javax.swing.SwingConstants.CENTER);
         btnAceptar.setIconTextGap(-3);
@@ -108,7 +108,7 @@ public class frmLogin extends javax.swing.JFrame {
         btnCancelar.setBorder(null);
         btnCancelar.setBorderPainted(false);
         btnCancelar.setContentAreaFilled(false);
-        btnCancelar.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+        btnCancelar.setCursor(new java.awt.Cursor(java.awt.Cursor.DEFAULT_CURSOR));
         btnCancelar.setFocusPainted(false);
         btnCancelar.setHorizontalTextPosition(javax.swing.SwingConstants.CENTER);
         btnCancelar.setIconTextGap(-2);
@@ -126,10 +126,11 @@ public class frmLogin extends javax.swing.JFrame {
 
         btnRecuperar.setFont(new java.awt.Font("Arial", 2, 14)); // NOI18N
         btnRecuperar.setForeground(new java.awt.Color(0, 51, 255));
-        btnRecuperar.setText("Olvidaste tu contraseña?");
+        btnRecuperar.setText("¿Olvidaste tu contraseña?");
         btnRecuperar.setBorder(null);
         btnRecuperar.setBorderPainted(false);
         btnRecuperar.setContentAreaFilled(false);
+        btnRecuperar.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
         btnRecuperar.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 btnRecuperarActionPerformed(evt);
